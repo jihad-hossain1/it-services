d1442fa48dd9e44d723bfe03832461e0bcd2f72c
kamruzzaman22874 <joybd.eee@gamil.com>
2024-03-11 14:52:02 +0600

    Footer section modify

---
+++ src/pages/Home/AppDevelopment/AppDevelopment.jsx
@@ -0,0 +1,11 @@
+const AppDevelopment = () => {
+    return (
+        <div className="text-center uppercase text-[#74B7FB] font-bold">
+            <h2 className="font-extrabold text-2xl">This is App Development page....</h2>
+            <h2 className="text-xl">Main Content Coming Soon.....</h2>
+        </div>
+
+    );
+};
+
+export default AppDevelopment;
